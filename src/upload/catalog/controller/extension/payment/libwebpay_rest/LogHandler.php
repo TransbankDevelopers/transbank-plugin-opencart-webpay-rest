28aca876b119253dde263f7348ff7279b52dd80c
DiegoHBustamante <99495937+DiegoHBustamante@users.noreply.github.com>
2024-10-02 13:36:45 -0300

    refactor: change require in LogHandler.php

--- src/upload/catalog/controller/extension/payment/libwebpay_rest/LogHandler.php
+++ src/upload/catalog/controller/extension/payment/libwebpay_rest/LogHandler.php
@@ -1,5 +1,5 @@
 <?php
-require __DIR__ . '/../../../../../vendor/autoload.php';
+require __DIR__ . '/../../../../../system/library/transbank/vendor/autoload.php';
 
 use Monolog\Logger;
 use Monolog\Handler\RotatingFileHandler;
