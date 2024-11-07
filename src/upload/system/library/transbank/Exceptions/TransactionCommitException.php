b1aca314b4d98d328d0955b79652d84074e0000f
DiegoHBustamante <99495937+DiegoHBustamante@users.noreply.github.com>
2024-11-07 09:23:45 -0300

    refactor: delete unused exception files

--- src/upload/system/library/transbank/Exceptions/TransactionCommitException.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Transbank\Exceptions;
-class TransactionCommitException extends \Exception
-{
-    public function __construct($message = "El token webpay es requerido", $code = 0, \Exception $previous = null)
-    {
-        parent::__construct($message, $code, $previous);
-    }
-}
