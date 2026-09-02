464dc4fc97767d0521f3eb3d659094701577897d
Francisca Díaz <francisca.diaz@continuumhq.com>
2026-09-02 12:05:42 -0400

    refactor: migrate diagnostic modal to native dialog element

--- src/upload/admin/view/javascript/webpay_rest/webpay_rest.js
+++ src/upload/admin/view/javascript/webpay_rest/webpay_rest.js
@@ -1,56 +1,79 @@
 $(document).ready(function() {
 
-    $('#tb_commerce_mod_info').on('show.bs.modal', function() {
-        $('.modal .modal-body').css('overflow-y', 'auto');
-        $('.modal .modal-body').css('max-height', $(window).height() * 0.7);
-    });
+    $('.open_commerce_modal').click(onOpenModalClick);
+    $('.close_commerce_modal').click(onCloseModalClick);
+    $('#tb_commerce_mod_info').on('click', onModalBackdropClick);
 
     $('#tb_logs').hide();
 
-    $('.tabInfo').click(function(e) {
-        var target = $(this).attr("href");
-        if (target == '#tb_main_info') {
-            $('#tb_main_info').show();
-            $('#tb_logs').hide();
-        } else {
-            $('#tb_main_info').hide();
-            $('#tb_logs').show();
-        }
-    });
-
-    $(".check_conn").click(function() {
-
-        var url = $(this).data('url');
-
-        $(".check_conn").text("Verificando ...");
-        $(".tbk_table_trans").empty();
-
-        $.post(url, {}, function(response){
-
-            $(".check_conn").text("Verificar Conexión");
-            $("#div_response_status").removeClass("tbk-hide");
-            $("#response_title").removeClass("tbk-hide");
-            $("#response_status_text").removeClass("label-success").removeClass("label-danger");
-
-            if(response.status.string == "OK") {
-
-                $("#response_status_text").addClass("label-success").text("OK").show();
-                $("#response_url_text").text(response.response.url);
-                $("#response_token_text").empty().append($('<pre>').text(response.response.token_ws));
-
-                $("#div_response_url").removeClass("tbk-hide");
-                $("#div_response_token").removeClass("tbk-hide");
-
-            } else {
-
-                $("#response_status_text").addClass("label-danger").text("ERROR").show();
-                $("#error_response_text").text(response.response.error);
-                $("#error_detail_response_text").empty().append($('<pre>').text(response.response.detail));
-
-                $("#div_error_response").removeClass("tbk-hide");
-                $("#div_error_detail_response").removeClass("tbk-hide");
-            }
-
-        },'json');
-    });
+    $('.tabInfo').click(onTabClick);
+    $(".check_conn").click(onCheckConnClick);
 });
+
+function onOpenModalClick() {
+    const dialog = document.getElementById('tb_commerce_mod_info');
+    dialog.showModal();
+    $('.modal-body', dialog).css('overflow-y', 'auto');
+    $('.modal-body', dialog).css('max-height', $(window).height() * 0.7);
+}
+
+function onCloseModalClick() {
+    document.getElementById('tb_commerce_mod_info').close();
+}
+
+function onModalBackdropClick(e) {
+    if (e.target === this) {
+        this.close();
+    }
+}
+
+function onTabClick() {
+    const target = $(this).attr("href");
+    if (target == '#tb_main_info') {
+        $('#tb_main_info').show();
+        $('#tb_logs').hide();
+    } else {
+        $('#tb_main_info').hide();
+        $('#tb_logs').show();
+    }
+}
+
+function onCheckConnClick() {
+    const url = $(this).data('url');
+
+    $(".check_conn").text("Verificando ...");
+    $(".tbk_table_trans").empty();
+
+    $.post(url, {}, onCheckConnResponse, 'json');
+}
+
+function onCheckConnResponse(response) {
+    $(".check_conn").text("Verificar Conexión");
+    $("#div_response_status").removeClass("tbk-hide");
+    $("#response_title").removeClass("tbk-hide");
+    $("#response_status_text").removeClass("label-success").removeClass("label-danger");
+
+    if (response.status.string == "OK") {
+        showCheckConnSuccess(response.response);
+    } else {
+        showCheckConnError(response.response);
+    }
+}
+
+function showCheckConnSuccess(data) {
+    $("#response_status_text").addClass("label-success").text("OK").show();
+    $("#response_url_text").text(data.url);
+    $("#response_token_text").empty().append($('<pre>').text(data.token_ws));
+
+    $("#div_response_url").removeClass("tbk-hide");
+    $("#div_response_token").removeClass("tbk-hide");
+}
+
+function showCheckConnError(data) {
+    $("#response_status_text").addClass("label-danger").text("ERROR").show();
+    $("#error_response_text").text(data.error);
+    $("#error_detail_response_text").empty().append($('<pre>').text(data.detail));
+
+    $("#div_error_response").removeClass("tbk-hide");
+    $("#div_error_detail_response").removeClass("tbk-hide");
+}
